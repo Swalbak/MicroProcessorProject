04456d786a8c0e82cc5c585155abd61a359361b3
swalbak <usually670@gmail.com>
2023-05-16 18:06:26 +0900

    w

--- add_user.py
+++ add_user.py
@@ -1,14 +1,33 @@
 import time
+import os
 import picamera
+import pickle
 
 CAPTURE_COUNT = 10
+# change to TTS, STT
 username = input("Type your name: ")
-if
-with picamera.PiCamera() as camera:
-	camera.resolution = (640, 480)
-	camera.start_preview()
-	
-	for i in range(CAPTURE_COUNT):
-		time.sleep(1)
-		camera.capture(f'jh{i}.jpg')
 
+if not os.path.isdir(f'./{username}'):
+	os.mkdir(f"./{username}")
+
+	user_images_select = []
+
+	with picamera.PiCamera() as camera:
+		camera.resolution = (640, 480)
+		camera.start_preview()
+		print("Capture start!!")
+
+		for i in range(CAPTURE_COUNT):
+			# capture image(save)
+			time.sleep(1)
+			camera.capture(f'./{username}/{username}{i}.jpg')
+			user_images_select.append(f"{username}{i}.jpg")
+		
+		print("Capture complete!!")
+
+	
+
+	
+
+else:
+	print("Already exist user.")
