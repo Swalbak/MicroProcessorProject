df33496dbf9b32d89096daa2b80109c6390cdc9e
Swalbak <usually670@gmail.com>
2023-05-17 00:34:09 +0900

    camera_main, add_user

--- add_user.py
+++ add_user.py
@@ -1,17 +1,33 @@
+import face_recognition
 import time
 import os
 import picamera
 import pickle
 
+'''
+add_user.py
+* 신규 유저 추가 모듈(인코딩 포함)
+
+user_names: 기존 유저들 이름 목록
+1. 유저별로 디렉토리 생성
+2. 해당 디렉토리에 사진 10장, 인코딩 정보 저장
+3. user_names에 새로 추가된 user 이름 추가
+'''
+
 CAPTURE_COUNT = 10
 # change to TTS, STT
 username = input("Type your name: ")
+
+# load user name list
+with open('user_names.pickle', 'rb') as f:
+	user_names = pickle.load(f)
 
 if not os.path.isdir(f'./{username}'):
 	os.mkdir(f"./{username}")
 
 	user_images_select = []
-
+	known_face_encodings = []
+	
 	with picamera.PiCamera() as camera:
 		camera.resolution = (640, 480)
 		camera.start_preview()
@@ -25,9 +41,27 @@ if not os.path.isdir(f'./{username}'):
 		
 		print("Capture complete!!")
 
-	
+	# make encoding list
+	for i in range(CAPTURE_COUNT):
+		user_image = user_images_select[i]
+		image = face_recognition.load_image_file(f"./{username}/{user_image}")
+		face_encoding = face_recognition.face_encodings(image)
 
-	
+		if face_encoding:
+			known_face_encodings.append(face_encoding[0])
+		else:
+			print(f"{user_images_select[i]} not encoded!!")
+
+	# encoding list dump
+	with open(f'./{username}/known_face_encodings.pickle', 'wb') as f:
+		pickle.dump(known_face_encodings, f)
+
+	# new username append
+	user_names.append(username)
+
+	# user_names list dump
+	with open('user_names.pickle', 'wb') as f:
+		pickle.dump(user_names, f)
 
 else:
 	print("Already exist user.")
---
+++ camera_main.py
@@ -0,0 +1,70 @@
+import face_recognition
+import numpy as np
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import pickle
+
+'''
+camera_main.py
+* 만들어진 유저 인코딩을 가져와 누구인지 판별하는 모듈
+'''
+
+# 사용자들의 사진을 불러옵니다.
+# user_images_select = ["용빈.jpg", "지준.jpg", "jh1.jpg", "jh2.jpg"]
+# user_names_select = ["yongb 1", "jj 2", "jh", "jh"]
+user_names = []
+
+# 사용자들의 얼굴 인코딩을 저장할 리스트를 만듭니다.
+known_face_encodings = []
+
+# 각 사용자의 사진에서 얼굴 인코딩을 추출합니다.
+# for i in range(len(user_images_select)):
+#     user_image = user_images_select[i]
+#     image = face_recognition.load_image_file(user_image)
+#     face_encoding = face_recognition.face_encodings(image)
+#     if face_encoding:
+#         known_face_encodings.append(face_encoding[0])
+#         user_names.append(user_names_select[i])
+#     else:
+#         print(f"{user_images_select[i]} not encoded!!")
+
+# PiCamera를 이용해 실시간으로 얼굴 인식을 진행합니다.
+
+# user name list
+with open('user_names.pickle', 'rb') as f:
+    user_names_select = pickle.load(f)
+
+# make encoding list
+for user in user_names_select:
+    with open(f'./{user}/known_face_encodings.pickle', 'rb') as f:
+        encoding = pickle.load(f)
+    known_face_encodings += encoding
+    user_names = user * len(encoding)
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+rawCapture = PiRGBArray(camera, size=(640, 480))
+camera.start_preview()
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
+
+    # 현재 카메라에 찍힌 이미지에서 얼굴 부분을 인식합니다.
+    face_locations = face_recognition.face_locations(image)
+    face_encodings = face_recognition.face_encodings(image, face_locations)
+
+    for face_encoding in face_encodings:
+        # 인식된 얼굴이 알려진 사용자들의 얼굴과 얼마나 유사한지 확인합니다.
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+
+        # 사용자가 일치하는 경우, 해당 사용자의 이름을 출력합니다.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = user_names[first_match_index]
+
+        print(f"The person in the video is: {name}")
+
+    rawCapture.truncate(0)
+
+    
